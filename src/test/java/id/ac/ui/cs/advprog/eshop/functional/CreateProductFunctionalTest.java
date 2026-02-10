fc3a0d6b6f1d5c42e4b5301d37396484b64f35ff
qowiys <qowiyshabir1416@gmail.com>
2026-02-10 20:09:34 +0700

    Add create product functional test

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
@@ -0,0 +1,49 @@
+package id.ac.ui.cs.advprog.eshop.functional;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(SeleniumJupiter.class)
+class CreateProductFunctionalTest {
+
+    @LocalServerPort
+    private int serverPort;
+
+    @Value("${app.baseUrl:http://localhost}")
+    private String testBaseUrl;
+
+    private String baseUrl;
+
+    @BeforeEach
+    void setupTest() {
+        baseUrl = String.format("%s:%d", testBaseUrl, serverPort);
+    }
+
+    @Test
+    void testUserCanCreateProductAndSeeItInProductList(ChromeDriver driver) {
+        driver.get(baseUrl + "/product/create");
+
+        driver.findElement(By.id("productName"))
+                .sendKeys("Buku Tulis");
+
+        driver.findElement(By.id("productQuantity"))
+                .sendKeys("10");
+
+        driver.findElement(By.tagName("button")).click();
+
+        assertTrue(driver.getPageSource().contains("Buku Tulis"));
+    }
+}
